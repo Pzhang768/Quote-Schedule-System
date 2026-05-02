774ccd8d11b4ce3f8344f152d6c8cf726d8c1b44
Paul Zhang <hzhang526@gmail.com>
2026-05-02 19:28:34 +1000

    feat(BR-11): add husky with commitlint and pre-commit hooks

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "BR-1", "BR-2", "BR-3", "BR-4", "BR-5",
+        "BR-6", "BR-7", "BR-8", "BR-9", "BR-10",
+        "BR-11", "BR-12", "BR-13", "BR-14",
+      ],
+    ],
+    "scope-case": [2, "always", "upper-case"],
+  },
+};
